2bb404cc2906c6d2ee3e891ada7f932d4e4866e5
anardil14 <emilce.pugliese@gmail.com>
2018-11-22 17:59:32 -0300

    Add allow host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')7lrizwxs*2$rehiot3pcwy@)tmq$@bbrp2lt#p*^&5$u^hb%l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<emilce>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'anardil.pythonanywhere.com']
 
 
 # Application definition
